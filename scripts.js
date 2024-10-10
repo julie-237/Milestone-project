24a0240a6722a634407f5ac71a05dd9ab1f3d9a3
unknown <blandinedjoumsie@gmail.com>
2024-10-10 05:15:39 +0200

    added eventlisteners to the edit buttons in order to open the overlay configuration panel, then added an eventlistener (submit) to the confirm button of the configuration panel

--- scripts.js
+++ scripts.js
@@ -1,2 +1,31 @@
-const editPlayer1ButtonElement = document.getElementById("edit-player1")
-const editPlayer2ButtonElement = document.getElementById("edit-player2")
+const editPlayer1ButtonElement = document.getElementById("edit-player1");
+const editPlayer2ButtonElement = document.getElementById("edit-player2");
+const configurationPanelElement = document.getElementById("side-form");
+
+function OpenConfigurationPanel() {
+  configurationPanelElement.style.display = "block";
+}
+
+editPlayer1ButtonElement.addEventListener("click", OpenConfigurationPanel);
+editPlayer2ButtonElement.addEventListener("click", OpenConfigurationPanel);
+
+const cancelButtonElement = document.getElementById("Cancel");
+
+function closeConfigurationPanel() {
+  configurationPanelElement.style.display = "none";
+}
+
+cancelButtonElement.addEventListener("click", closeConfigurationPanel);
+
+const formElement = document.querySelector("form");
+function getPlayerName(event) {
+  event.preventDefault();
+  const formdata = new FormData(event.target);
+  const enteredPlayerName = formdata.get("user-name");
+  console.log(enteredPlayerName)
+  // const inputElement = document.querySelector("input")
+  //const playerNameElement = document.getElementById("edit-player1")
+  // playerNameElement.textContent = inputElement.textContent
+}
+
+formElement.addEventListener("submit", getPlayerName);
