60f988691e8948f4b09ef8d11de5eed7b1e1f5bb
unknown <blandinedjoumsie@gmail.com>
2024-10-14 18:12:39 +0200

    displayed the player name entered by the player to the configuration panel using dataset object and properties

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ const editPlayer1ButtonElement = document.getElementById("edit-player1");
 const editPlayer2ButtonElement = document.getElementById("edit-player2");
 const configurationPanelElement = document.getElementById("side-form");
 
-function OpenConfigurationPanel() {
+let editedplayer = 0;
+function OpenConfigurationPanel(event) {
+  editedplayer = +event.target.dataset["playerId"]; // + to change value type from string to interger
   configurationPanelElement.style.display = "block";
 }
 
@@ -13,9 +15,8 @@ const cancelButtonElement = document.getElementById("Cancel");
 
 function closeConfigurationPanel() {
   configurationPanelElement.style.display = "none";
-  formElement.children[1].classList.remove("error")
+  formElement.children[1].classList.remove("error");
   errorOutputElement.textContent = "";
-
 }
 
 cancelButtonElement.addEventListener("click", closeConfigurationPanel);
@@ -23,15 +24,31 @@ cancelButtonElement.addEventListener("click", closeConfigurationPanel);
 const formElement = document.querySelector("form");
 const errorOutputElement = document.getElementById("warning");
 
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
 function getPlayerName(event) {
   event.preventDefault();
   const formdata = new FormData(event.target);
   const enteredPlayerName = formdata.get("user-name").trim();
   if (!enteredPlayerName) {
     errorOutputElement.textContent = "Please enter a valid name!";
-    event.target.children[1].classList.add("error")
+    event.target.children[1].classList.add("error");
     return;
   }
+  const updatedPlayerDataElement = document.getElementById(
+    "player" + editedplayer + "-name"
+  );
+  updatedPlayerDataElement.textContent = enteredPlayerName;
+  closeConfigurationPanel();
 }
 
 formElement.addEventListener("submit", getPlayerName);
