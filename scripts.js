18066cd14d3dfa735c3813b33f2f9867df4bd91e
unknown <blandinedjoumsie@gmail.com>
2024-10-16 05:27:57 +0200

    checked for a winner or a draw

--- scripts.js
+++ scripts.js
@@ -84,7 +84,7 @@ const gameData = [
   [0, 0, 0],
   [0, 0, 0],
 ];
-
+let currentRound = 1;
 let activePlayer = 0;
 const activePlayerNameElement = document.getElementById("active-player-name");
 
@@ -114,6 +114,11 @@ function selectgamefield(event) {
   }
   gameData[selectedColumn][selectedrow] = activePlayer + 1;
   console.log(gameData);
+
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
+  
+  currentRound++;
   switchPlayer();
 }
 
@@ -121,3 +126,49 @@ function selectgamefield(event) {
 // gameFieldElement.addEventListener("click", selectgamefield);
 //}
 gameBoardElement.addEventListener("click", selectgamefield);
+
+function checkForGameOver() {
+  // checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Diagonal: Bottom left to top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+  if (currentRound === 9) {
+    return -1;
+  }
+  return 0;
+}
